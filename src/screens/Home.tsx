782d69f30c250b7f0c690d120db4b53f12b0e417
Rodrigo <rodrigorael53@gmail.com>
2022-07-07 17:55:55 -0300

    creating a button to take the user to screenA

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+// na primeira posição do generic(<>) eu falo qual tipagem das minhas rotas
+// na segunda posição eu falo qual é a inteface que eu estou agora
+// type StackProps = NativeStackScreenProps<StackParamList, 'Home'>;
 
 export function Home() {
+  function openScreen() {
+    const navigation = useNavigation();
+    navigation.navigate('screenA');
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: 'red' }}>
+    <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center' }}>
+
+      <Button 
+        title="Ir para tela A"
+        onPress={openScreen}
+      />
 
     </View>
   );
